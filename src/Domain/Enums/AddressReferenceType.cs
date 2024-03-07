ba2d87544b182bac93a07f1a4ab1a2b4831336c1
daveconc23 <daveconcepcion31@gmail.com>
2024-03-07 09:43:56 +0800

    Update and Add Enums Helper

---
+++ src/Domain/Enums/AddressReferenceType.cs
@@ -0,0 +1,13 @@
+﻿namespace DMS.Domain.Enums
+{
+    public class AddressReferenceType
+    {
+        public enum Index
+        {
+            Company = 1,
+            Vendor = 2,
+            Customer = 3,
+            Employee = 4
+        }
+    }
+}
---
+++ src/Domain/Enums/ModuleCodes2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Domain.Enums
+{
+    public static class ModuleCodes2
+    {
+        //File Setup
+        public const string CONST_HOME = "HOME";
+
+        public const string CONST_COMP = "COMP";
+        public const string CONST_MODULE = "MODULE";
+        public const string CONST_USERROLE = "USERROLE";
+        public const string CONST_ROLE_MGMT = "ROLE";
+        public const string CONST_APPLICANTS = "APLCNT";
+        public const string CONST_PROFILE = "PROFILE";
+        public const string CONST_FAQS = "FAQS";
+        public const string CONST_HLF068 = "HLF068";
+        public const string CONST_DOCUMENT = "DOCUMENT";
+        public const string CONST_BENEFICIARY = "BENEFICIARY";
+        public const string CONST_APPLICANTSREQUESTS = "APLCNTREQ";
+        public const string CONST_DocumentUpload = "DOCUPLOAD";
+    }
+}
