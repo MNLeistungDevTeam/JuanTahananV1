cf1dc9bd2e653c1491d95190a221c3a1ab6ee94e
Leo Pern Paniza <leopern.paniza@mnleistung.de>
2024-03-10 13:11:46 +0800

    refresh db context

--- src/Domain/Entities/CompanyLogo.cs
+++ src/Domain/Entities/CompanyLogo.cs
@@ -5,17 +5,25 @@ using System.Collections.Generic;
 
 namespace DMS.Domain.Entities;
 
-public partial class ModeOfPayment
+public partial class CompanyLogo
 {
     public int Id { get; set; }
 
     public string Description { get; set; }
 
-    public DateTime DateCreated { get; set; }
+    public string Location { get; set; }
+
+    public bool IsDisabled { get; set; }
 
     public int CreatedById { get; set; }
+
+    public DateTime DateCreated { get; set; }
 
     public int? ModifiedById { get; set; }
 
     public DateTime? DateModified { get; set; }
+
+    public int? CompanyId { get; set; }
+
+    public virtual Company Company { get; set; }
 }
