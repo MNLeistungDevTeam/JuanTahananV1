9e86e94e7b18ef3b2006a0c0840cc0a82e473f72
Leo Pern Paniza <leopern.paniza@mnleistung.de>
2024-03-22 10:22:17 +0800

    update on get total info

--- src/Database/Stored Procedures/Setup/ApplicantsPersonalInformation/spApplicantsPersonalInformation_GetTotalInfo.sql
+++ src/Database/Stored Procedures/Setup/ApplicantsPersonalInformation/spApplicantsPersonalInformation_GetTotalInfo.sql
@@ -4,17 +4,21 @@ CREATE PROCEDURE [dbo].[spApplicantsPersonalInformation_GetTotalInfo]
 @userId INT
 AS
  SELECT
+	-- GET Submitted/Pending Review Count
     COUNT(CASE WHEN ApprovalStatus = 1 THEN 1 END) OVER () AS TotalPendingReview,
-    COUNT(CASE WHEN ApprovalStatus = 2 THEN 1 END) OVER () AS TotalApprove,
-    COUNT(CASE WHEN ApprovalStatus = 3 THEN 1 END) OVER () AS TotalDisApprove
+	-- GET Pagibig Verified Count
+    COUNT(CASE WHEN ApprovalStatus = 4 THEN 1 END) OVER () AS TotalApprove,
+	-- GET Deferred Count
+    COUNT(CASE WHEN ApprovalStatus = 2 THEN 1 END) OVER () AS TotalDisApprove
 FROM
     ApplicantsPersonalInformation
 
     WHERE  1 = (
-	CASE WHEN @userId IS NULL THEN 1
-	WHEN @userId = 0 THEN 1
-	WHEN @userId IS NOT NULL AND @userId = UserId THEN 1
-	END
+		CASE 
+			WHEN @userId IS NULL THEN 1
+			WHEN @userId = 0 THEN 1
+			WHEN @userId IS NOT NULL AND @userId = UserId THEN 1
+		END
 	)
 
 RETURN 0
