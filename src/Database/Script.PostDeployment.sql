514da03b00b4e7ca37d48d7c17c5f8c4830bdfd1
daveconc23 <daveconcepcion31@gmail.com>
2024-03-22 08:11:40 +0800

    Fix Missing Field on Barrowers and Spouse Industry in HLF

--- src/Database/Script.PostDeployment.sql
+++ src/Database/Script.PostDeployment.sql
@@ -25,3 +25,4 @@ Post-Deployment Script Template
 :r .\PostDeploymentScript\Script.PostDeployment_ModeOfPayment.sql
 :r .\PostDeploymentScript\Script.PostDeployment_Company.sql
 :r .\PostDeploymentScript\Script.PostDeployment_SourcePagibigFund.sql
+:r .\PostDeploymentScript\Script.PostDeployment_Industry.sql
