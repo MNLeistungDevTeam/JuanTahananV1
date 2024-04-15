70d6b1cc9ce0b4adbd9ed4c7bdc3cf1b3acaf534
Marc Nelson Belasa <marcnelsonbelasa008@gmail.com>
2024-04-15 14:19:29 +0800

    BeneficiaryScript small changes

--- src/Web/wwwroot/js/Setup/Applicants/BeneficiaryScript.js
+++ src/Web/wwwroot/js/Setup/Applicants/BeneficiaryScript.js
@@ -117,7 +117,7 @@ $(function () {
                     }
 
                     else if (row.ApprovalStatusNumber == 9) {
-                        returndata = ` <span class="badge fs-6 border bg-secondary">${data}</span> `;
+                        returndata = ` <span class="badge fs-6 border bg-danger">${data}</span> `;
                     }
                     else if (row.ApprovalStatusNumber == 10) {
                         returndata = ` <span class="badge fs-6 border bg-warning">${data}</span> `;
