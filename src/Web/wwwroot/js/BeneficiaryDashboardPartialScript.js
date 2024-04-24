104d090b1c3313424001ef1b2a427264b9435992
jcaracas117 <91721108+jcaracas117@users.noreply.github.com>
2024-04-24 13:32:21 +0800

    Beneficiary Dashboard: Javascript - Decluttering Load function

--- src/Web/wwwroot/js/BeneficiaryDashboardPartialScript.js
+++ src/Web/wwwroot/js/BeneficiaryDashboardPartialScript.js
@@ -67,27 +67,29 @@ $(() => {
             success: function (data) {
                 if (data.ApplicationStatus === null) {
                     $(`[id="custom_ribbon"]`).addClass('d-none');
+                    //$(`[id="with_application"]`)
+                    //return;
                 }
 
-                let color = classColors.find(a => a.approvalStatusNumbers.includes(data.ApprovalStatus)).classColor;
+                let color = classColors.find(a => a.approvalStatusNumbers.includes(data.ApprovalStatus));
+                let selectedColor = color ? color.classColor : "bg-primary"
                 //console.log(color);
 
                 // Greeting
                 // $(`[id="user_first_name"]`).html(data.ApplicantFirstName);
-                //
+                //  
 
-                $(`[id="status_contract"]`).addClass(color);
-                $(`[id="status_contract"]`).html(data.ApplicationStatus);
+                $(`[id="status_contract"]`).addClass(selectedColor);
+                $(`[id="status_contract"]`).html(data.ApplicationStatus || "No Application");
 
                 $(`[id="reference_number"]`).html(data.Code || "-----");
                 $(`[id="requested_loan_amount"]`).html(data.LoanAmount !== 0 ? numeral(data.LoanAmount).format('0, 0.00') : "------"); // use numeral
                 $(`[id="loan_term"]`).html(data.LoanYears !== 0 ? data.LoanYears : "--"); // use numeral
                 $(`[id="project_location"]`).html(data.ProjectLocation || "-----");
 
-                let appStatus = data.Stage || "Unknown";
-                let appStatusRemarks;
-
-                console.log(appStatus);
+                let appStatus = data.Stage || "No Application";
+                let appStatusRemarks = "Kindly proceed submitting an application";
+                let statusColor = `secondary`;
 
                 $(`[id="beneficiary_sidecard"]`).removeClass('bg-warning');
                 $(`[id="application_status"]`).removeClass('text-warning');
@@ -95,36 +97,27 @@ $(() => {
                 // set color
                 if ([0, 1, 2, 3, 5, 11].includes(data.ApprovalStatus)) {
                     // `Credit Verification`
-                    $(`[id="beneficiary_sidecard"]`).addClass('bg-warning');
-                    $(`[id="application_status"]`).addClass('text-warning');
-
+                    statusColor = "warning";
                     appStatusRemarks = `Stage ends after Pag-IBIG credit verification`;
                 }
                 else if ([4, 6, 7, 9, 10].includes(data.ApprovalStatus)) {
                     // `Application Completion`
-                    $(`[id="beneficiary_sidecard"]`).addClass('bg-primary');
-                    $(`[id="application_status"]`).addClass('text-primary');
-
+                    statusColor = "primary";
                     appStatusRemarks = `Stage ends after Pag-IBIG approval`;
                 }
                 else if (data.ApprovalStatus === 8) {
                     // `Post-Approval`
-                    $(`[id="beneficiary_sidecard"]`).addClass('bg-success');
-                    $(`[id="application_status"]`).addClass('text-success');
-
+                    statusColor = "success";
                     appStatusRemarks = `Stage ends after disbursement of loan`;
                 }
 
+                $(`[id="beneficiary_sidecard"]`).addClass(`bg-${statusColor}`);
+                $(`[id="application_status"]`).addClass(`text-${statusColor}`);
                 $(`[id="beneficiary_sidecard"] [id="beneficiary_sidecard_text"]`).html(appStatus);
                 $(`[id="application_status"]`).html(appStatus);
                 $(`[id="application_status_remarks"]`).html(appStatusRemarks);
 
-
-
-
-
-
-
+                // Credit History
                 $(`[id="credit_history_status"]`).removeClass('text-success');
                 $(`[id="credit_history_remarks"]`).removeClass('text-muted');
 
