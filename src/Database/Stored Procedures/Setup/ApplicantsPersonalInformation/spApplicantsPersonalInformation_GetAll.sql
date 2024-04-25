fdcd4270eed14a00e6d09aa99cbc01b12e2695c8
daveconc23 <daveconcepcion31@gmail.com>
2024-04-25 17:32:58 +0800

    update Application Table Labels and Add SubDocuments Table

--- src/Database/Stored Procedures/Setup/ApplicantsPersonalInformation/spApplicantsPersonalInformation_GetAll.sql
+++ src/Database/Stored Procedures/Setup/ApplicantsPersonalInformation/spApplicantsPersonalInformation_GetAll.sql
@@ -7,10 +7,10 @@ AS
 		CONCAT(u.LastName,', ',u.FirstName,' ',u.MiddleName) ApplicantFullName,
 		u.[Position] PositionName,  --applicant position
 		0.00 As IncomeAmount,
-		bi.PropertyDeveloperName Developer,
-		bi.PropertyLocation ProjectLocation,
+		bfi.PropertyDeveloperName Developer,
+		bfi.PropertyLocation ProjectLocation,
 		'' Project,
-		bi.PropertyUnitLevelName Unit,
+		bfi.PropertyUnitLevelName Unit,
 		lpi.DesiredLoanAmount As LoanAmount,
 		--CASE WHEN apl.ApprovalStatus = 1 Then 'Application in Draft'
 		--	 WHEN  apl.ApprovalStatus = 2 Then 'Approved'
@@ -49,6 +49,7 @@ AS
 		aps.Remarks  
 	FROM ApplicantsPersonalInformation apl
 	LEFT JOIN BarrowersInformation bi ON bi.ApplicantsPersonalInformationId = apl.Id
+	LEFT JOIN BeneficiaryInformation bfi ON bfi.UserId = apl.UserId
 	LEFT JOIN LoanParticularsInformation lpi ON lpi.ApplicantsPersonalInformationId = apl.Id
 	LEFT JOIN [User] u on u.Id = apl.UserId
 	LEFT JOIN [UserRole] ur ON ur.UserId = u.Id
