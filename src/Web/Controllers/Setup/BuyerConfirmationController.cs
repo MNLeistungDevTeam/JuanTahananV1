bfe49b546498ded334a716f68bcbc3d3d7e2717f
daveconc23 <daveconcepcion31@gmail.com>
2024-05-03 15:22:06 +0800

    Ready BCF (incomplete)

---
+++ src/Web/Controllers/Setup/BuyerConfirmationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DMS.Web.Controllers.Setup
+{
+    public class BuyerConfirmationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
