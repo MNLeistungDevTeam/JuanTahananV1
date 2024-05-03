9455cf46e3e8af6dfaf44e208e5f08a0929b4eb7
daveconc23 <daveconcepcion31@gmail.com>
2024-05-03 08:27:44 +0800

    fix sorting

--- src/Database/Stored Procedures/Setup/ApplicantsPersonalInformation/spApplicantsPersonalInformation_GetAll.sql
+++ src/Database/Stored Procedures/Setup/ApplicantsPersonalInformation/spApplicantsPersonalInformation_GetAll.sql
@@ -109,7 +109,7 @@ AS
 
 
   
-  SELECT * FROM (
+    SELECT * FROM (
   
  SELECT main.*,aplog2.DateCreated DateSubmitted ,  ROW_NUMBER() OVER (PARTITION BY aps2.ReferenceId ORDER BY aplog2.DateCreated ASC) AS rn
 FROM (
@@ -208,31 +208,16 @@ FROM (
 	LEFT JOIN ApprovalStatus aps2 ON main.Id = aps2.ReferenceId
 	LEFT JOIN ApprovalLog aplog2 ON aplog2.ReferenceId = aps2.ReferenceId and aplog2.[Action] = 1 and aplog2.CreatedById = aps2.UserId
 	) MAIN2
-	WHERE MAIN2.rn = 1
+	WHERE MAIN2.rn = 1 
 
-
- ORDER BY
-    CASE
-        WHEN @roleId IN (1, 2, 5, 3) THEN MAIN2.Id
-        WHEN @roleId = 4 THEN 
-            CASE 
-                WHEN MAIN2.ApprovalStatus = 0 THEN 1 
-                ELSE 0 
-            END
-        ELSE 0
-    END DESC,
-    CASE
-        WHEN @roleId IN (1, 2, 5, 3) THEN MAIN2.DateModified
-        ELSE NULL
-    END DESC,
-    CASE
-        WHEN @roleId IN (1, 2, 5, 3) THEN NULL
-        ELSE MAIN2.datecreated
-    END ASC,
-    CASE
-        WHEN @roleId IN (1, 2, 5, 3) THEN NULL
-        ELSE MAIN2.datemodified
-    END ASC;
+ ORDER BY 
+    CASE 
+        WHEN ApprovalStatus = 0 THEN 0 -- Rows with ApprovalStatus = 0 come first
+        ELSE 1 -- All other rows
+    END,
+    -- Add additional sorting criteria as needed
+    MAIN2.LastUpdated DESC; -- For example, sorting by DateCreated in descending order
+ 
 
 
 
