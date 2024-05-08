3968c1647d4b9275e961b9a08de6dbbe85ae5d0b
daveconc23 <daveconcepcion31@gmail.com>
2024-05-08 16:28:21 +0800

    apply sleect value on radiobutton when edit mode

--- src/Web/wwwroot/js/Setup/Applicants/HLF068-BeneficiaryScript.js
+++ src/Web/wwwroot/js/Setup/Applicants/HLF068-BeneficiaryScript.js
@@ -1777,6 +1777,61 @@ $(function () {
         let bouncingChecksValue = $("[name='Form2PageModel.BouncingChecks']").val();
         let medicalAdviceValue = $("[name='Form2PageModel.MedicalAdvice']").val();
 
+        let pagibigAvailedLoan = $("[name='BuyerConfirmationModel.IsPagibigAvailedLoan']").val();
+
+        let coborrower = $("[name='BuyerConfirmationModel.IsPagibigCoBorrower']").val();
+
+        let projectProponent = $("[name='BuyerConfirmationModel.IsPursueProjectProponent']").val();
+        let termConditions = $("[name='BuyerConfirmationModel.IsInformedTermsConditions']").val();
+
+        let isPagibigMember = $("[name='BuyerConfirmationModel.IsPagibigMember']").val();
+        let isOtherSourceIncome = $("[name='BuyerConfirmationModel.IsOtherSourceOfIncome']").val();
+
+        // If pagibigAvailedLoan has a value of "1", set the radio button as checked
+        if (pagibigAvailedLoan === "True") {
+            $("#availedLoanRbtn1").prop("checked", true);
+        } else {
+            $("#availedLoanRbtn2").prop("checked", true);
+        }
+
+        // If pagibigAvailedLoan has a value of "1", set the radio button as checked
+        if (coborrower === "True") {
+            $("#cbwrRbtn1").prop("checked", true);
+        } else {
+            $("#cbwrRbtn2").prop("checked", true);
+        }
+
+        // If pagibigAvailedLoan has a value of "1", set the radio button as checked
+        if (projectProponent === "True") {
+            $("#prpRbtn1").prop("checked", true);
+        } else {
+            $("#prpRbtn2").prop("checked", true);
+        }
+
+        // If pagibigAvailedLoan has a value of "1", set the radio button as checked
+        if (termConditions === "True") {
+            $("#itcRbtn1").prop("checked", true);
+        } else {
+            $("#itcRbtn2").prop("checked", true);
+        }
+
+        // If pagibigAvailedLoan has a value of "1", set the radio button as checked
+        if (isPagibigMember === "True") {
+            $("#pagibigRbtn1").prop("checked", true);
+        } else {
+            $("#pagibigRbtn2").prop("checked", true);
+        }
+
+        if (isOtherSourceIncome === "True") {
+            $("#isRbtn1").prop("checked", true);
+        } else {
+            $("#isRbtn2").prop("checked", true);
+        }
+
+        // Set checked status for PendingCase radio buttons
+        $("#pcRadioBtn1").prop("checked", !!pendingCaseValue);
+        $("#pcRadioBtn2").prop("checked", !pendingCaseValue);
+
         if (applicantInfoIdVal !== '0') {
             // Set checked status for PendingCase radio buttons
             $("#pcRadioBtn1").prop("checked", !!pendingCaseValue);
