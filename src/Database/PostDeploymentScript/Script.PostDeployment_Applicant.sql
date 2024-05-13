1ab651b1d4443ae9050aa2e2780726a11c4b562e
daveconc23 <daveconcepcion31@gmail.com>
2024-05-13 09:03:59 +0800

    update deployment script add only beneficiary no application yet

--- src/Database/PostDeploymentScript/Script.PostDeployment_Applicant.sql
+++ src/Database/PostDeploymentScript/Script.PostDeployment_Applicant.sql
@@ -1,35 +1,35 @@
 ﻿ 
 
- IF NOT EXISTS (SELECT 1 FROM [dbo].ApplicantsPersonalInformation)
-BEGIN
-SET IDENTITY_INSERT ApplicantsPersonalInformation ON;
+-- IF NOT EXISTS (SELECT 1 FROM [dbo].ApplicantsPersonalInformation)
+--BEGIN
+--SET IDENTITY_INSERT ApplicantsPersonalInformation ON;
 
--- Insert dummy data into ApplicantsPersonalInformation
-INSERT INTO ApplicantsPersonalInformation
-    (Id, Code, UserId, PagibigNumber, CompanyId, ApprovalStatus, DateCreated,CreatedById)
-VALUES
-    (1, 'APL202401-0001', 4, '324135645768', 1, 0, GETDATE(),1);
+---- Insert dummy data into ApplicantsPersonalInformation
+--INSERT INTO ApplicantsPersonalInformation
+--    (Id, Code, UserId, PagibigNumber, CompanyId, ApprovalStatus, DateCreated,CreatedById)
+--VALUES
+--    (1, 'APL202401-0001', 4, '324135645768', 1, 0, GETDATE(),1);
 
  
-SET IDENTITY_INSERT ApplicantsPersonalInformation OFF;
-END 
-GO
+--SET IDENTITY_INSERT ApplicantsPersonalInformation OFF;
+--END 
+--GO
  
- IF NOT EXISTS (SELECT 1 FROM [dbo].BarrowersInformation)
-BEGIN
-SET IDENTITY_INSERT BarrowersInformation ON;
+-- IF NOT EXISTS (SELECT 1 FROM [dbo].BarrowersInformation)
+--BEGIN
+--SET IDENTITY_INSERT BarrowersInformation ON;
 
--- Insert dummy data into BarrowersInformation
-INSERT INTO BarrowersInformation
-    (Id, ApplicantsPersonalInformationId, LastName, FirstName, MiddleName, Sex, PagibigMidNumber, Email,CreatedById)
-VALUES
-    (1, 1, 'Cortel', 'Albert', 'La Vina', 'Male', '324135645768', 'beneficiary@email.com',1);
+---- Insert dummy data into BarrowersInformation
+--INSERT INTO BarrowersInformation
+--    (Id, ApplicantsPersonalInformationId, LastName, FirstName, MiddleName, Sex, PagibigMidNumber, Email,CreatedById)
+--VALUES
+--    (1, 1, 'Cortel', 'Albert', 'La Vina', 'Male', '324135645768', 'beneficiary@email.com',1);
     
 
--- Disable identity insert for BarrowersInformation
-SET IDENTITY_INSERT BarrowersInformation OFF;
-END 
-GO
+---- Disable identity insert for BarrowersInformation
+--SET IDENTITY_INSERT BarrowersInformation OFF;
+--END 
+--GO
 
 
 
@@ -51,17 +51,17 @@ END
 GO
 
 
- IF NOT EXISTS (SELECT 1 FROM [dbo].ApprovalStatus)
-BEGIN
-SET IDENTITY_INSERT ApprovalStatus ON;
+-- IF NOT EXISTS (SELECT 1 FROM [dbo].ApprovalStatus)
+--BEGIN
+--SET IDENTITY_INSERT ApprovalStatus ON;
  
  
-INSERT INTO ApprovalStatus
-(Id,UserId,ReferenceId,ReferenceType,[Status],LastUpdate)
-VALUES(1,4,1,8,1,GETDATE())
+--INSERT INTO ApprovalStatus
+--(Id,UserId,ReferenceId,ReferenceType,[Status],LastUpdate)
+--VALUES(1,4,1,8,1,GETDATE())
  
-SET IDENTITY_INSERT ApprovalStatus OFF;
-END 
-GO
+--SET IDENTITY_INSERT ApprovalStatus OFF;
+--END 
+--GO
 
 
