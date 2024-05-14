8bd82600eda6fa6e3a241401b2059882e6f055fc
jcaracas117 <91721108+jcaracas117@users.noreply.github.com>
2024-05-14 10:29:51 +0800

    BCF Upload User interface update

--- src/Web/wwwroot/js/Setup/BuyerConfirmation/BuyerConfirmationUploadScript.js
+++ src/Web/wwwroot/js/Setup/BuyerConfirmation/BuyerConfirmationUploadScript.js
@@ -1,32 +1,88 @@
-"use strict"
+﻿"use strict"
 
 $(function () {
-    ////////////////////////////////
+    var bcfDropzone;
+    let selectedFile;
 
-    $(`[id="bcf_PdfFile"]`).fileinput({
-        dropZoneTitle: `
-            <div class="file-icon">
-                <i class="mdi mdi-file-pdf"></i>
-            </div>
-            <p class="fw-4">
-                <span class="text-info fw-bold">Drag and Drop</span>
-                <br>
-                here to upload your file or
-                <br>
-                browse files using the button below.
-            </p>
-        `,
-        showPreview: true,
-        allowedFileExtensions: ['pdf'],
-        maxFileCount: 1,
-        minFileCount: 1,
-        maxFileSize: (1024 * 5),
-        theme: "fa5",
-        browseClass: "btn btn-info",
-        mainClass: "d-grid",
-        showCaption: false,
-        showRemove: true,
-        showUpload: false,
+    ////////////////////////////////
+    initializeBsFileInput();
+    //initializeTraditionalDrop();
+
+    $(`[id="submitPdfFile"]`).on('click', function (e) {
+        e.preventDefault();
     });
 
+    function initializeBsFileInput() {
+        $(`[id="bcf_PdfFile"]`).fileinput({
+            dropZoneTitle: `
+                <div class="file-icon">
+                    <img src="${baseUrl}img/pdf.png" />
+                </div>
+                <p class="fw-4">
+                    <span class="text-info fw-bold">Drag and Drop</span>
+                    <br>
+                    here to upload your file or
+                    <br>
+                    browse files using the button below.
+                </p>
+            `,
+            showPreview: true,
+            allowedFileExtensions: ['pdf'],
+            maxFileCount: 1,
+            minFileCount: 1,
+            maxFileSize: (1024 * 5),
+            theme: "explorer-fa5",
+            browseClass: "btn btn-info flex-grow-1",
+            removeClass: "btn btn-danger flex-grow-1",
+            browseLabel: "Browse",
+            mainClass: "d-flex gap-1 justify-content-center",
+            showCaption: false,
+            showRemove: true,
+            showUpload: false,
+            removeFromPreviewOnError: true
+        });
+    }
+
+    function initializeTraditionalDrop() {
+        const dropArea = document.querySelector(".file-drag-area");
+
+        dropArea.addEventListener("dragover", (event) => {
+            event.preventDefault();
+
+            // file is over dragarea
+            dropArea.classList.add("active");
+        });
+
+        dropArea.addEventListener("dragleave", () => {
+            // file is outside dragarea
+            dropArea.classList.remove("active");
+        });
+        
+        dropArea.addEventListener("drop", (event) => {
+            event.preventDefault();
+
+            // file is dropped on dragarea
+            dropArea.classList.remove("active");
+
+            selectedFile = event.dataTransfer.files[0];
+            //console.log(file.type);
+
+            let validExtensions = ["application/pdf"];
+
+            if (validExtensions.includes(selectedFile.type)) {
+                console.log("pdf");
+
+                // proceed to place file
+                let fileReader = new FileReader();
+
+                fileReader.onload = () => {
+                    let fileUrl = fileReader.result;
+
+                    
+                };
+            }
+
+        });
+
+    }
 });
