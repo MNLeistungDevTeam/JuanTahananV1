682df8b211dee1bf3baf5ddab92cec991162eeb0
daveconc23 <daveconcepcion31@gmail.com>
2024-05-16 16:39:02 +0800

    fixing duplication of approval on document

--- src/Database/Stored Procedures/Setup/BuyerConfirmation/spBuyerConfirmation_GetAll.sql
+++ src/Database/Stored Procedures/Setup/BuyerConfirmation/spBuyerConfirmation_GetAll.sql
@@ -2,28 +2,49 @@
 AS
 	 
 
-	 	SELECT 
-		bcf.Code,
-		CONCAT(bcf.LastName, ', ',bcf.FirstName, ' ',bcf.MiddleName) ApplicantFullName,
-		bcf.PagibigNumber,
-		bcf.MonthlySalary,
-		bcf.ProjectProponentName,
-		bcf.HouseUnitModel,
-		CASE
-			WHEN bcf.ApprovalStatus = 0 THEN 'Application in Draft'
-			WHEN bcf.ApprovalStatus = 3 AND bcd.[Status] = 1  THEN 'Sign and Submitted'
-			WHEN bcf.ApprovalStatus = 3 AND bcd.[Status] = 11 THEN 'Resubmission' --document resubmit
-			WHEN bcf.ApprovalStatus = 3 AND bcd.[Status] = 3  THEN 'Approved'
-			WHEN bcf.ApprovalStatus = 3 THEN 'Ready For Printing'
-			WHEN bcf.ApprovalStatus = 11 THEN 'For Revision'
-		END ApplicationStatus,
-		bcf.ApprovalStatus,
-		bcf.DateCreated,
-		bcf.DateModified,
-		d.[Name] [FileName],
-		d.[Location] FileLocation,
-		bcf.Id  BuyerConfirmationDocumentId
-	FROM BuyerConfirmation bcf
-	LEFT JOIN Document d ON d.ReferenceNo = bcf.Code  And DocumentTypeId = 0
-	LEFT JOIN BuyerConfirmationDocument bcd ON bcd.ReferenceId = d.Id and bcd.[Status] != 11
+	  SELECT 
+	 bcd.Status,
+    bcf.Code,
+    CONCAT(bcf.LastName, ', ', bcf.FirstName, ' ', bcf.MiddleName) AS ApplicantFullName,
+    bcf.PagibigNumber,
+    bcf.MonthlySalary,
+    bcf.ProjectProponentName,
+    bcf.HouseUnitModel,
+    CASE
+        WHEN bcf.ApprovalStatus = 0 THEN 'Application in Draft'
+        WHEN bcf.ApprovalStatus = 3 AND bcd.[Status] = 1 THEN 'Sign and Submitted'
+        WHEN bcf.ApprovalStatus = 3 AND bcd.[Status] = 11 THEN 'For Resubmission' -- document resubmit
+        WHEN bcf.ApprovalStatus = 3 AND bcd.[Status] = 3 THEN 'Approved'
+        WHEN bcf.ApprovalStatus = 3 THEN 'Ready For Printing'
+        WHEN bcf.ApprovalStatus = 11 THEN 'For Revision'
+    END AS ApplicationStatus,
+    bcf.ApprovalStatus,
+    bcf.DateCreated,
+    bcf.DateModified,
+	CASE WHEN bcd.[Status] = 11 Then
+	'' ELSE d.[Name]
+	END [FileName],
+	CASE WHEN bcd.[Status] = 11 Then
+	'' ELSE d.[Location]
+	END FileLocation,
+    bcd.Id AS BuyerConfirmationDocumentId,
+	CASE WHEN bcd.[Status] = 11 Then
+	0 ELSE bcd.Id
+	END BuyerConfirmationDocumentId
+FROM BuyerConfirmation bcf
+LEFT JOIN (
+    SELECT 
+        bcd1.*,
+        ROW_NUMBER() OVER (PARTITION BY bcd1.ReferenceNo ORDER BY bcd1.DateCreated DESC) AS rn
+    FROM BuyerConfirmationDocument bcd1
+) bcd ON bcd.ReferenceNo = bcf.Code AND bcd.rn = 1
+LEFT JOIN Document d ON d.Id = bcd.ReferenceId
+
 RETURN 0
+
+ 
+ select * from BuyerConfirmationDocument
+
+
+ truncate table Document
+ truncate table BuyerConfirmationDocument
