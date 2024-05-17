2845835498f651d45bcbc01631f23eed2686179a
daveconc23 <daveconcepcion31@gmail.com>
2024-05-17 12:44:33 +0800

    update document script

--- src/Database/PostDeploymentScript/Script.PostDeployment_DocumentType.sql
+++ src/Database/PostDeploymentScript/Script.PostDeployment_DocumentType.sql
@@ -1,12 +1,12 @@
 ﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[DocumentType])
 BEGIN
 SET IDENTITY_INSERT [dbo].[DocumentType] ON 
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES (1, N'PAGIBIG-BCNFIRM', N'PAGIBIG BUYERS CONFIRMATION', CAST(N'2024-02-24T08:31:43.3808888' AS DateTime2), 1, NULL, NULL, NULL, NULL)
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES (2,  N'PROOF-INCM',N'PROOF OF INCOME', CAST(N'2024-02-24T08:31:57.0094831' AS DateTime2), 1, NULL, NULL, NULL, NULL)
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES (3, N'PAGIBIG-HLAW/RCNTVALID-ID', N'PAGIBIG HOUSING LOAN APPLICATION WITH RECENT VALID ID OF BORROWER & CO-BORROWER', CAST(N'2024-02-24T08:32:05.9640793' AS DateTime2), 1, NULL, NULL, NULL, NULL)
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (4, N'BC', N'BIRTH CERTIFICATE', CAST(N'2024-02-24T08:32:12.9360798' AS DateTime2), 1, NULL, NULL, NULL, NULL)
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (5, N'MC', N'MARRIAGE CERTIFICATE OR CENOMAR', CAST(N'2024-02-24T08:32:21.6886051' AS DateTime2), 1, NULL, NULL, NULL, NULL)
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (6, N'CD', N'CEDULA', CAST(N'2024-02-24T08:32:32.3003803' AS DateTime2), 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES (1, N'PAGIBIG-HLF', N'Pag-IBIG Housing Loan Application (HLF)', CAST(N'2024-02-24T08:31:43.3808888' AS DateTime2), 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES (2,  N'PROOF-INCM',N'Proof Of Income', CAST(N'2024-02-24T08:31:57.0094831' AS DateTime2), 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES (3, N'PAGIBIG-HLAW/RCNTVALID-ID', N'Recent valid ID (borrower & co-borrower) together w/ the HLF', CAST(N'2024-02-24T08:32:05.9640793' AS DateTime2), 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (4, N'BC', N'Birth Certificate', CAST(N'2024-02-24T08:32:12.9360798' AS DateTime2), 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (5, N'MC', N'Marriage Certificate Or Cenomar', CAST(N'2024-02-24T08:32:21.6886051' AS DateTime2), 1, NULL, NULL, NULL, NULL)
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (6, N'CD', N'Cedula', CAST(N'2024-02-24T08:32:32.3003803' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (7, N'ID', N'1 X 1 ID PICTURE', CAST(N'2024-02-24T08:32:44.9771157' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (8, N'TCT', N'TCT (TRANSFER CERTIFICATE OF TITLE)', CAST(N'2024-02-24T08:32:59.0298253' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (9, N'CCT', N'CCT (CONDOMINIUM CERTIFICATE OF TITLE)', CAST(N'2024-02-24T08:33:06.3514489' AS DateTime2), 1, CAST(N'2024-02-24T08:33:46.5876187' AS DateTime2), 1, NULL, NULL)
@@ -23,12 +23,15 @@ INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedBy
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (20, N'CR-PCA', N'WLF182 CHECKLIST REQUIREMENTS PRELIMINARY CONDOMINIUM APPRAISAL', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL)
                                                                                                                                                          
 
+
+
+
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (21, N'HLA-V', N'HLF1035 Housing Loan Application (V01)', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (22, N'HLA-CB', N'HLF1036 Housing Loan Application Co-borrower', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (23, N'HLF1042', N'Borrower-Beneficiary Conformity', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (24, N'HLF1046', N'Authority to Deduct 4PH', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL)
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (25, N'HLF1069', N'Conformity Non-relatives 4PH', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL)
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (26, N'WLF252', N'Buyer Confirmation Form', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL);
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById]) VALUES  (26, N'WLF252', N'Pag-IBIG Buyers Confirmation (BCF)', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL);
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (27, N'PYSL', N'Payslip', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (28, N'COE', N'Certificate Of Employment', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (29, N'ITR', N'Latest Income Tax Return', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
@@ -37,7 +40,13 @@ INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedBy
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (32, N'CLC-TD', N'Copy of Lease Contact and Tax Declaration', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (33, N'CTC-TF', N'Certified True Copy of Transport Franchise', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
 INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (34, N'COENGMNT', N'Certificate of Engagement', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
-INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (35, N'', N'Statement of pressumed income to be certified by the barangay', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (35, N'ST-BRGY', N'Statement of pressumed income to be certified by the barangay', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (36, N'HLF-1X1PIC', N'1 x 1 ID Picture (Attached to HLF)', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,2);
+
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (37, N'4PH-HLF', N'4PH Housing Loan Application Form', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,NULL);
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (38, N'4PH-HLF-CBRROWER', N'4PH Housing Loan Application Co-borrower', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,NULL);
+INSERT [dbo].[DocumentType] ([Id],Code, [Description], [DateCreated], [CreatedById], [DateModified], [ModifiedById], [DateDeleted], [DeletedById],[ParentId]) VALUES  (39, N'BCF', N'Buyer Confirmation Form', CAST(N'2024-02-24T08:35:48.1374701' AS DateTime2), 1, NULL, NULL, NULL, NULL,NULL);
+
 SET IDENTITY_INSERT [dbo].[DocumentType] OFF
 END
 GO
@@ -48,32 +57,34 @@ BEGIN
 	SET IDENTITY_INSERT [dbo].DocumentVerification ON
 INSERT INTO [dbo].[DocumentVerification] ([Id], DocumentTypeId, [Type], CreatedById, DateCreated, ModifiedById, DateModified)
 VALUES 
-    (1, 1, 1, 1, GETDATE(), NULL, NULL),
-    (2, 2, 1, 1, GETDATE(), NULL, NULL), 
-    (3, 3, 1, 1, GETDATE(), NULL, NULL), 
-    (4, 4, 1, 1, GETDATE(), NULL, NULL), 
-    (5, 5, 1, 1, GETDATE(), NULL, NULL), 
-    (6, 6, 1, 1, GETDATE(), NULL, NULL), 
-    (7, 7, 1, 1, GETDATE(), NULL, NULL),
+    (1, 1, 1, 1, GETDATE(), NULL, NULL),-- pagibig hloan
+    (2, 3, 1, 1, GETDATE(), NULL, NULL), --recent valid id w coborrower
+    (3, 26, 1, 1, GETDATE(), NULL, NULL), -- pagibig bcf
+    (4, 4, 1, 1, GETDATE(), NULL, NULL), --b certificate
+    (5, 5, 1, 1, GETDATE(), NULL, NULL),  --cenomar
+    (6, 6, 1, 1, GETDATE(), NULL, NULL),  --cedula
+    (7, 36, 1, 1, GETDATE(), NULL, NULL), -- . 1 x 1 ID Picture (Attached to HLF)
+
+    (8, 27, 1, 1, GETDATE(), NULL, NULL),  -- PAYSLIP
+    (9, 28, 1, 1, GETDATE(), NULL, NULL),  -- Coe
+    (10, 29, 1, 1, GETDATE(), NULL, NULL), -- itr
+
+    (11, 30, 1, 1, GETDATE(), NULL, NULL),  -- commision vouccher
+    (12, 31, 1, 1, GETDATE(), NULL, NULL),  -- bank 12 months
+    (13, 32, 1, 1, GETDATE(), NULL, NULL), -- copy lease tax decla
+    (14, 33, 1, 1, GETDATE(), NULL, NULL), -- transport frnachise
+    (15, 34, 1, 1, GETDATE(), NULL, NULL), -- coe
+    (16, 35, 1, 1, GETDATE(), NULL, NULL), -- presume income
 
 
-    (8, 21, 2, 1, GETDATE(), NULL, NULL), 
-    (9, 22, 2, 1, GETDATE(), NULL, NULL), 
-    (10, 23, 2, 1, GETDATE(), NULL, NULL),
-    (11, 24, 2, 1, GETDATE(), NULL, NULL), 
-    (12, 25, 2, 1, GETDATE(), NULL, NULL), 
-    (13, 26, 2, 1, GETDATE(), NULL, NULL),
-    (14, 27, 1, 1, GETDATE(), NULL, NULL), 
-    (15, 28, 1, 1, GETDATE(), NULL, NULL), 
-    (16, 29, 1, 1, GETDATE(), NULL, NULL), 
-    (17, 30, 1, 1, GETDATE(), NULL, NULL), 
-    (18, 31, 1, 1, GETDATE(), NULL, NULL), 
-    (19, 32, 1, 1, GETDATE(), NULL, NULL), 
-    (20, 33, 1, 1, GETDATE(), NULL, NULL), 
-    (21, 34, 1, 1, GETDATE(), NULL, NULL), 
-    (22, 35, 1, 1, GETDATE(), NULL, NULL); 
+    (17, 37, 2, 1, GETDATE(), NULL, NULL),  -- 4PH  Housing Loan Application Form
+    (18, 38, 2, 1, GETDATE(), NULL, NULL),  -- 4PH Housing Loan Application Co-borrower 
+    (19, 23, 2, 1, GETDATE(), NULL, NULL), -- Borrower-Beneficiary Conformity
+    (20, 24, 2, 1, GETDATE(), NULL, NULL), --  Authority to Deduct
+    (21, 25, 2, 1, GETDATE(), NULL, NULL), -- Conformity Non-relatives
+    (22, 39, 2, 1, GETDATE(), NULL, NULL); -- Buyer Confirmation Form
 
- 
+
 SET IDENTITY_INSERT [dbo].DocumentVerification OFF
 
 END
@@ -98,8 +109,10 @@ VALUES
     (5, 30, 2, 2, GETDATE(), NULL), 
     (6, 31, 2, 2, GETDATE(), NULL), 
     (7, 32, 2, 2, GETDATE(), NULL), 
+
     (8, 33, 2, 2, GETDATE(), NULL), 
     (9, 34, 2, 2, GETDATE(), NULL), 
+
     (10, 35, 3, 2, GETDATE(), NULL); 
 
  
