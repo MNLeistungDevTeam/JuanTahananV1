0dc5994a3d3fce2569953bd309384c7276f1c68c
daveconc23 <daveconcepcion31@gmail.com>
2024-05-17 13:36:49 +0800

    added bcfsummary template excel

--- src/Web/Controllers/Setup/BuyerConfirmationController.cs
+++ src/Web/Controllers/Setup/BuyerConfirmationController.cs
@@ -7,7 +7,9 @@ using DMS.Application.Interfaces.Setup.UserRepository;
 using DMS.Domain.Dto.BuyerConfirmationDto;
 using DMS.Web.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,15 +26,17 @@ public class BuyerConfirmationController : Controller
     private readonly IUserRepository _userRepo;
     private readonly IBeneficiaryInformationRepository _beneficiaryInformationRepo;
     private readonly IBuyerConfirmationRepository _buyerConfirmationRepo;
+    private readonly IWebHostEnvironment _webhost;
 
     public BuyerConfirmationController(
         IUserRepository userRepo,
         IBeneficiaryInformationRepository beneficiaryInformationRepo,
-        IBuyerConfirmationRepository buyerConfirmationRepo)
+        IBuyerConfirmationRepository buyerConfirmationRepo, IWebHostEnvironment webhost)
     {
         _userRepo = userRepo;
         _beneficiaryInformationRepo = beneficiaryInformationRepo;
         _buyerConfirmationRepo = buyerConfirmationRepo;
+        _webhost = webhost;
     }
 
     #endregion Fields
@@ -198,100 +202,37 @@ public class BuyerConfirmationController : Controller
         }
     }
 
-
-
-
-
     public async Task<IActionResult> BCFSummary()
     {
         try
         {
-            int companyId = int.Parse(User.FindFirstValue("Company"));
-
-            //var vendor = await _fileSetupDA.GetVendors(companyId);
-
-            //if (!vendor.Any())
-            //    return BadRequest("No Record Found!");
-
+            var rootFolder = _webhost.WebRootPath;
             string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            string templatePath = Path.Combine(rootFolder, "Files", "ExcelTemplate", "Prequalified_BuyerConfirmation_Summary.xlsx");
+
             string fileName = $"BCFSummary.xlsx";
 
-            using var workbook = new XLWorkbook();
-            IXLWorksheet worksheet = workbook.Worksheets.Add("BCFSummary");
-            worksheet.Style.Font.FontSize = 9;
-            worksheet.Style.Font.FontName = "Arial";
+            int companyId = int.Parse(User.FindFirstValue("Company"));
 
+            // Load the Excel template
+            using (var workbook = new XLWorkbook(templatePath))
+            {
+                // Get the first worksheet in the workbook
+                var worksheet = workbook.Worksheets.Worksheet(1);
 
+                // Example modification: Set the value of cell A1
+                worksheet.Cell("B5").Value = "Yuhum Residences";
 
+                // Save the modified workbook to a MemoryStream
+                using (var memoryStream = new MemoryStream())
+                {
+                    workbook.SaveAs(memoryStream);
 
-            worksheet.Cell(5, 1).Value = "PROJECT PROPONENT'S NAME:";
-            worksheet.Cell(6, 1).Value = "PROJECT NAME:";
-            worksheet.Cell(7, 1).Value = "LOCATION:";
-
-            worksheet.Cell(5, 14).Value = "DEVELOPER / CONTRACTOR:";
-            worksheet.Cell(6, 14).Value = "ESTIMATED NO. OF UNITS:";
-            worksheet.Cell(7, 14).Value = "TOTAL NO. OF PRE-QUALIFIED BENEFICIARIES:";
-
-
-
-            worksheet.Cell(9, 1).Value = "Project Beneficiary Details";
-            worksheet.Cell(9, 2).Value = "Employment/Business Details";
-
-            worksheet.Cell(9, 3).Value = "No.";
-
-            worksheet.Cell(9, 4).Value = "Name";
-
-            worksheet.Cell(9, 5).Value = "If Pag-IBIG Member \r\n(Yes / No)";
-
-            worksheet.Cell(9, 6).Value = "Pag-IBIG \r\nMID Number";
-            worksheet.Cell(9, 7).Value = "Birthday\r\n(mm/dd/yyyy)";
-            worksheet.Cell(9, 9).Value = "Gross Monthly Income\r\n(GMI)";
-            worksheet.Cell(9, 10).Value = "Present Home Address";
-            worksheet.Cell(9, 11).Value = "Contact Number/s";
-            worksheet.Cell(9, 12).Value = "Email Address";
-            worksheet.Cell(9, 13).Value = "Employer's/Business Name";
-            worksheet.Cell(9, 14).Value = "Contact Person";
-            worksheet.Cell(9, 15).Value = "Contact Number/s";
-            worksheet.Cell(9, 16).Value = "Email Address";
-
-
-
-
-            //int lstndx = 0;
-            //int index = 1;
-
-            //List<string> debit_total = new();
-            //List<string> credit_total = new();
-
-            //foreach (var item in vendor)
-            //{
-            //    worksheet.Cell(index + 1, 1).Value = item.VendorCode;
-            //    worksheet.Cell(index + 1, 2).Value = item.VendorName;
-            //    worksheet.Cell(index + 1, 3).Value = item.CurrentBalance;
-            //    worksheet.Cell(index + 1, 4).Value = item.Address?.ToString().Trim().Normalize();
-            //    worksheet.Cell(index + 1, 5).Value = item.Representative;
-            //    worksheet.Cell(index + 1, 6).Value = item.TransactionCount;
-            //    worksheet.Cell(index + 1, 7).Value = item.Inactive ? "Inactive" : "Active";
-
-            //    lstndx = index + 1;
-
-            //    index++;
-            //}
-            //IXLRange range2 = worksheet.Range(worksheet.Cell(2, 3).Address, worksheet.Cell(lstndx, 3).Address);
-            //range2.Style.NumberFormat.Format = "#,##0.00;(#,##0.00);-;";
-
-            //IXLRange range2 = worksheet.Range(worksheet.Cell(2, 2).Address, worksheet.Cell(lstndx, 3).Address);
-            //range2.Style.NumberFormat.Format = "#,##0.00;(#,##0.00);-;";
-
-            worksheet.Columns().AdjustToContents();
-            worksheet.Rows().AdjustToContents();
-
-            //worksheet.Range($"B3:E{lstndx + 2}").Style.Border.OutsideBorder = XLBorderStyleValues.Medium;
-
-            using var stream = new MemoryStream();
-              workbook.SaveAs(stream);
-            var content =  stream.ToArray();
-            return File(content, contentType, fileName);
+                    var content = memoryStream.ToArray();
+                    return File(content, contentType, fileName);
+                }
+            }
         }
         catch (Exception ex)
         {
@@ -299,11 +240,6 @@ public class BuyerConfirmationController : Controller
         }
         // End of Action
     }
-
-
-
-
-
 
     #endregion API Operation
 }
