9d802101cd4b0b459ec1ff0bfe86a9087ecb477d
daveconc23 <daveconcepcion31@gmail.com>
2024-05-20 18:08:22 +0800

    add Cors policy on program.cs, added new template for hlf, rebuild and test api for jtahanan portal

---
+++ src/Database/Stored Procedures/Setup/PropertyUnit/spPropertyUnit_GetUnitByProjectId.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spPropertyUnit_GetUnitByProjectId]
+	@projectId INT,
+	@developerId INT
+AS
+SELECT * FROM PropertyUnit pu
+ LEFT JOIN
+ (SELECT DISTINCT pup.UnitId,pp.CompanyId,pp.Id ProjectId  FROM
+   PropertyUnitProject pup
+
+   LEFT JOIN PropertyProject pp ON pp.Id = pup.ProjectId 
+   )pr ON pr.UnitId = pu.Id
+   
+   WHERE pr.CompanyId = @developerId AND
+    1 = CASE WHEN @projectId IS NULL THEN 1 
+              WHEN @projectId IS NOT NULL AND @projectId = pr.ProjectId  THEN 1
+		  END
+RETURN 0
