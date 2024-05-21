f97303a28c62c72e94ee38c1615ad4f2fea8833c
daveconc23 <daveconcepcion31@gmail.com>
2024-05-21 17:48:57 +0800

    added ContactDetailEmail fields

--- src/Domain/Dto/ApplicantsDto/BarrowersInformationModel.cs
+++ src/Domain/Dto/ApplicantsDto/BarrowersInformationModel.cs
@@ -257,6 +257,7 @@ namespace DMS.Domain.Dto.ApplicantsDto
 
         public bool IsBcfCreated { get; set; }
 
+        [Display(Name="Mailing Address/Contact Detail")]
         public string? ContactDetailEmail { get;set; } 
     }
 }
