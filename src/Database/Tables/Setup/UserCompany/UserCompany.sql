060461a4271b806fef51368040ab90052c1b8aef
daveconc23 <daveconcepcion31@gmail.com>
2024-05-25 23:41:53 +0800

    added usercompany table for segragation of developer user per company

---
+++ src/Database/Tables/Setup/UserCompany/UserCompany.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserCompany]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	UserId INT NOT NULL,
+	CompanyId INT NOT NULL,
+	CreatedById INT NULL,
+	DateCreated DATETIME2(7) NULL DEFAULT(GETDATE()),
+	ModifiedById INT NULL,
+	DateModified DATETIME2(7)
+)
