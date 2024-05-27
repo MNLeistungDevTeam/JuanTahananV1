e91c56717a8d02aff569125720659916dbc1a9ce
daveconc23 <daveconcepcion31@gmail.com>
2024-05-27 08:52:00 +0800

    fix conflict on deployment

--- src/Database/PostDeploymentScript/Script.PostDeployment_PropertyManagement.sql
+++ src/Database/PostDeploymentScript/Script.PostDeployment_PropertyManagement.sql
@@ -16,11 +16,10 @@ INSERT [dbo].[PropertyLocation] ([Id],[Name],DateCreated,CreatedById,DateModifie
 END
 GO
 
-
- IF NOT EXISTS (SELECT 1 FROM [dbo].[PropertyUnit])
+   IF NOT EXISTS (SELECT 1 FROM [dbo].[PropertyUnit])
 BEGIN
 SET IDENTITY_INSERT [dbo].[PropertyUnit] ON 
-INSERT [dbo].[PropertyUnit] ([Id],[Name],[Description],DateCreated,DateModified,ModifiedById) VALUES (1, N'Single House', N'A 24 sqm With 4 Bed Room',CAST(N'2024-02-24T08:31:43.3808888' AS DateTime2), 1, NULL,NULL)
+INSERT [dbo].[PropertyUnit] ([Id],[Name],[Description],DateCreated,CreatedById,DateModified,ModifiedById) VALUES (1, N'Single House', N'A 24 sqm With 4 Bed Room',CAST(N'2024-02-24T08:31:43.3808888' AS DateTime2), 1, NULL,NULL)
  
  SET IDENTITY_INSERT [dbo].[PropertyUnit] OFF
 END
--- src/Database/PostDeploymentScript/Script.PostDeployment_User.sql
+++ src/Database/PostDeploymentScript/Script.PostDeployment_User.sql
@@ -1,31 +1,34 @@
 ﻿ IF NOT EXISTS (SELECT 1 FROM [dbo].[User])
 BEGIN
+    -- Enable IDENTITY_INSERT
     SET IDENTITY_INSERT [dbo].[User] ON;
 
-    INSERT [dbo].[User] (
+    -- Insert statements
+    INSERT INTO [dbo].[User] (
         [Id],
         [UserName],
         [Password],
         [PasswordSalt],
         [LastName],
-        [FirstName], 
+        [FirstName],
         [MiddleName],
-        [Email], 
+        [Email],
         [Position],
         [IsDisabled],
         [CreatedById],
-        [DateCreated], 
+        [DateCreated],
         [DateModified],
         [IsDark],
         [PagibigNumber]
     ) 
     VALUES 
-        (1, N'Admin', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', 'Super', 'Admin', '', N'admin@email.com', 'Admin', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL),
-        (2, N'LGU', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', 'Oliver', 'Jay pee', 'L', N'lgu@email.com', 'LGU', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL),
-        (3, N'Pagibig', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', 'Rosales', 'Mike', 'C', N'Pagibig@email.com', 'Pag-ibig', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL),
-        (4, N'Beneficiary', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', 'Cortel', 'Albert', 'La Viña', N'beneficiary@email.com', 'Beneficiary', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, '324135645768'),
-        (5, N'Developer', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', 'Concepcion', 'David', 'C', N'developer@email.com', 'Developer', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL);
-    
+        (1, N'Admin', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', N'Super', N'Admin', N'', N'admin@email.com', N'Admin', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL),
+        (2, N'LGU', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', N'Oliver', N'Jay pee', N'L', N'lgu@email.com', N'LGU', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL),
+        (3, N'Pagibig', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', N'Rosales', N'Mike', N'C', N'Pagibig@email.com', N'Pag-ibig', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL),
+        (4, N'Beneficiary', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', N'Cortel', N'Albert', N'La Viña', N'beneficiary@email.com', N'Beneficiary', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, N'324135645768'),
+        (5, N'Developer', N'b5lj1xrAEY90IM7voc61Dg8HDyREnU2mwQTl8sozfA8=', N'your-password-salt', N'Concepcion', N'David', N'C', N'developer@email.com', N'Developer', 0, 1, CAST(N'2023-05-31T10:35:44.1310781' AS DateTime2), NULL, 0, NULL);
+
+    -- Disable IDENTITY_INSERT
     SET IDENTITY_INSERT [dbo].[User] OFF;
 END;
 GO
